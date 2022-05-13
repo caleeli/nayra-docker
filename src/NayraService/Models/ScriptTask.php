d2a8566340e877a1f340007abdac246093351310
David Callizaya <davidcallizaya@gmail.com>
2022-05-13 16:06:09 -0400

    Implement run script/next state actions

---
+++ src/NayraService/Models/ScriptTask.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace ProcessMaker\NayraService\Models;
+
+use Exception;
+use ProcessMaker\Nayra\Bpmn\Models\ScriptTask as ModelsScriptTask;
+use ProcessMaker\Nayra\Contracts\Bpmn\ActivityInterface;
+use ProcessMaker\Nayra\Contracts\Bpmn\MultiInstanceLoopCharacteristicsInterface;
+use ProcessMaker\Nayra\Contracts\Bpmn\TokenInterface;
+
+class ScriptTask extends ModelsScriptTask
+{
+    /**
+     * Runs the ScriptTask
+     * @param TokenInterface $token
+     */
+    public function runScript(TokenInterface $token)
+    {
+        //if the script runs correctly complete te activity, otherwise set the token to failed state
+        if ($this->executeScript($token, $this->getScript())) {
+            $this->complete($token);
+        } else {
+            $token->setStatus(ActivityInterface::TOKEN_STATE_FAILING);
+        }
+    }
+
+    /**
+     * Script runner fot testing purposes that just evaluates the sent php code
+     *
+     * @param TokenInterface $token
+     * @param string $script
+     * @return bool
+     */
+    private function executeScript(TokenInterface $token, $script)
+    {
+        $result = true;
+        try {
+            $element = $token->getOwnerElement();
+            $loop = $element->getLoopCharacteristics();
+            $isMulti = $loop instanceof MultiInstanceLoopCharacteristicsInterface && $loop->isExecutable();
+            if ($isMulti) {
+                $data = $token->getProperty('data', []);
+            } else {
+                $data = $token->getInstance()->getDataStore()->getData();
+            }
+            $newData = eval('?>' . $script);
+            if (gettype($newData) === 'array') {
+                $data = array_merge($data, $newData);
+                if ($isMulti) {
+                    $token->setProperty('data', $data);
+                } else {
+                    $token->getInstance()->getDataStore()->setData($data);
+                }
+            }
+        } catch (Exception $e) {
+            $result = false;
+        }
+        return $result;
+    }
+}
--- src/NayraService/Request.php
+++ src/NayraService/Request.php
@@ -3,7 +3,6 @@
 namespace ProcessMaker\NayraService;
 
 use ProcessMaker\Nayra\Bpmn\Models\DataStore;
-use ProcessMaker\Nayra\Engine\ExecutionInstance;
 use ProcessMaker\Nayra\Storage\BpmnDocument;
 
 class Request
@@ -16,10 +15,16 @@ class Request
      * @var DataStore
      */
     public $dataStore;
+
     /**
-     * @var ExecutionInstance
+     * @var Instance[]
      */
-    public $instance;
+    public $instances;
+
+    /**
+     * @var Token[]
+     */
+    private $tokens = [];
 
     /**
      * @var Engine
@@ -28,33 +33,127 @@ class Request
 
     public function __construct($attributes)
     {
-        // $this->tokens = $attributes['tokens'];
-        // $this->status = $attributes['status'];
-
         $engine = new Engine();
+        if (isset($attributes['engine_id'])) {
+            $engine->engine_id = $attributes['engine_id'];
+        }
         $repository = $engine->getRepository();
         $bpmnDoc = new BpmnDocument();
         $bpmnDoc->setEngine($engine);
         $bpmnDoc->setFactory($repository);
         $bpmnDoc->load('./bpmn/' . $attributes['bpmn']);
+        $bpmnDoc->deploy_name = $attributes['bpmn'];
         $this->bpmn = $bpmnDoc;
         $this->engine = $engine;
 
-        // if (isset($attributes['tokens'])) {
-        //     $instance = $repository->createExecutionInstanceRepository()->createExecutionInstance();
-        //     $instance->setDataStore($dataStore);
-        // }
+        if (isset($attributes['callback'])) {
+            $this->engine->callbackUrl = $attributes['callback'];
+        }
+
+        if (isset($attributes['transactions'])) {
+            $state = $attributes['state'];
+            foreach ($attributes['transactions'] as $transaction) {
+                switch ($transaction['type']) {
+                    case 'create':
+                        switch ($transaction['entity']) {
+                            case 'request':
+                                $state['requests'] = $transaction['data'];
+                                break;
+
+                            case 'token':
+                                # code...
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+        if (isset($attributes['state'])) {
+            $requests = $attributes['state']['requests'] ?? [];
+            foreach ($requests as $request) {
+                $process = $bpmnDoc->getProcess($request['callable_id']);
+                $dataStore = $this->engine->getRepository()->createDataStore();
+                $dataStore->setData($request['data'] ?? []);
+                $instance = $repository->createExecutionInstance();
+                $this->engine->loadProcess($process);
+                $instance->setDataStore($dataStore);
+                $instance->setProcess($process);
+                $instance->setId($request['id']);
+                $process->addInstance($instance);
+                $this->engine->addInstance($instance);
+                // Add tokens
+                $tokens = $request['tokens'] ?? [];
+                foreach ($tokens as $token) {
+                    $tokenInstance = $repository->createTokenInstance();
+                    $tokenInstance->setProperties($token ?? []);
+                    $element = $bpmnDoc->findElementById($token['element_id'])->getBpmnElementInstance();
+                    $tokenInstance->setInstance($instance);
+                    $element->addToken($instance, $tokenInstance);
+                    $this->tokens[$tokenInstance->getId()] = $tokenInstance;
+                }
+                $this->instances[$instance->getId()] = $instance;
+            }
+        }
     }
 
     public function startProcess(array $params)
     {
-        if ($params['process_id']) {
+        if (isset($params['process_id'])) {
             $dataStore = $this->engine->getRepository()->createDataStore();
             $dataStore->setData($params['data'] ?? []);
             $this->dataStore = $dataStore;
             $process = $this->bpmn->getProcess($params['process_id']);
-            $this->instance = $process->call($this->dataStore);
+            $instance = $process->call($this->dataStore);
+            $instance->setProperty('extra_properties', $params['extra_properties'] ?? []);
             $this->engine->runToNextState();
         }
+        if (isset($params['element_id'])) {
+            $dataStore = $this->engine->getRepository()->createDataStore();
+            $dataStore->setData($params['data'] ?? []);
+            $this->dataStore = $dataStore;
+            $start = $this->bpmn->getStartEvent($params['element_id']);
+            $process = $start->getProcess();
+            $instance = $this->engine->createExecutionInstance($process, $dataStore);
+            $instance = $start->start($instance);
+            $this->engine->runToNextState();
+        }
+    }
+
+    public function completeTask(array $params)
+    {
+        if (isset($params['element_id'])) {
+            $token = $this->tokens[$params['token_id']];
+            $task = $this->bpmn->getActivity($params['element_id']);
+            foreach ($params['data'] as $key => $value) {
+                $token->getInstance()->getDataStore()->putData($key, $value);
+            }
+            $this->engine->runToNextState();
+            $task->complete($token);
+            $this->engine->runToNextState();
+        }
+    }
+
+    public function runScript(array $params)
+    {
+        if (isset($params['element_id'])) {
+            $token = $this->tokens[$params['token_id']];
+            $task = $this->bpmn->getScriptTask($params['element_id']);
+            $task->runScript($token);
+            $this->engine->runToNextState();
+        }
+    }
+
+    public function nextState(array $params)
+    {
+        if (isset($params['transactions'])) {
+            foreach ($params['transactions'] as $transaction) {
+            }
+        }
+        $this->engine->runToNextState();
+    }
+
+    public function callback(array $params)
+    {
+        $this->engine->callback($params);
     }
 }
